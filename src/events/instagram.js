82759f91f7a5565f8a4ed1122dea5532baa593b1
Ollie <github@ollie2304.co.uk>
2024-06-21 15:38:06 +0100

    add instagram embeds

--- src/events/instagram.js
+++ src/events/instagram.js
@@ -1,9 +1,30 @@
-// TODO add Instagram
 module.exports = (client) => {
     // Listens for a message to be sent
     client.on("messageCreate", async (message) => {
         // Ignore messages sent from bots
-       // if (message.author.bot) return;
+       if (message.author.bot) return;
+       const instagramRegex = /(instagram\.com\/\S+\/+)/g;
+       const content = message.content;
+       
+         if (
+              instagramRegex.test(content) &&
+              !content.includes("https://ddinstagram.com")
+            ) {
+              const editedContent = content.replace(instagramRegex, "dd$1");
+              try {
+                const webhook = await message.guild.channels.cache
+                  .get(message.channel.id)
+                  .createWebhook({
+                    name: message.member.displayName || message.author.username,
+                    avatar: message.author.displayAvatarURL({ dynamic: true }),
+                  });
+                await webhook.send(editedContent);
+                await webhook.delete();
+                await message.delete();
+              } catch (error) {
+                console.error(`Error with message: ${message.content}`, error);
+              }
+            }
     })
 };
 
